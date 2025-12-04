38de85061cf03854653c75b264b40202dd1f5872
Kartikey1884 <kartikeyrai8400@gmail.com>
2025-12-05 02:34:57 +0530

    batch prediction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from schemas import InputSchema, OutputSchema
-from predict import make_predict
+from predict import make_predict, make_batch_predict
+from typing import List
 
 app = FastAPI()
 
@@ -12,3 +13,8 @@ def index():
 def predict_price(input_data: InputSchema):
     prediction = make_predict(input_data.model_dump())
     return OutputSchema(predicted_price=round(prediction,2))
+
+@app.post('/batch_prediction',response_model=list[OutputSchema])
+def batch_predict(user_data:List[InputSchema]):
+    predictions=make_batch_predict([data.model_dump() for data in user_data])
+    return [OutputSchema(predicted_price=round(pred,2)) for pred in predictions]
--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import joblib
 import numpy as np
+from typing import List
 
 saved_model = joblib.load('housing_model.pkl')
 print("loaded the model from housing_model.pkl")
@@ -17,3 +18,17 @@ def make_predict(data:dict)->float:
             data['median_income']
       ]])
       return saved_model.predict(features)[0]
+
+
+def make_batch_predict(data:List[dict])->List[float]:
+      x = np.array([[
+            x['longitude'],
+            x['latitude'],
+            x['housing_median_age'],
+            x['total_rooms'],
+            x['total_bedrooms'],
+            x['population'],
+            x['households'],
+            x['median_income']
+      ] for x in data])
+      return saved_model.predict(x)
